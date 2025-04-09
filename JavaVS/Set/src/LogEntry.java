3ec7ba1b94269ae0529ec299fa61e20f757bdc1f
Guilherme <guimeygui@gmail.com>
2025-04-09 15:50:21 -0300

    Usando Set,Hash e equals

---
+++ JavaVS/Set/src/LogEntry.java
@@ -0,0 +1,55 @@
+import java.util.Date;
+
+public class LogEntry {
+    private String username;
+    private Date momento;
+
+
+    public LogEntry(String username, Date momento) {
+        this.username = username;
+        this.momento = momento;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public Date getMomento() {
+        return momento;
+    }
+    public void setMomento(Date momento) {
+        this.momento = momento;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        LogEntry other = (LogEntry) obj;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
+
+    
+
+
+
+
+    
+}
